c629d91492130d2132d0bdec8cd9fe0712397f3e
Mateo <131885138+fckyeslol@users.noreply.github.com>
2025-09-27 23:30:40 -0500

    added the mapa

--- camera_2d.gd
+++ camera_2d.gd
@@ -2,5 +2,4 @@ extends Camera2D
 
 func _ready():
 	enabled = true
-	make_current()
 	zoom = Vector2(1.5, 1.5)
--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,29 +1,11 @@
 extends CharacterBody2D
 
-@onready var anim_sprite = $AnimatedSprite2D
+var direction: float =0.0
+var speed: float= 1000.0
 
-var speed := 200  # Velocidad de movimiento
-
-func _physics_process(delta: float) -> void:
-	var direction := Vector2.ZERO
-
-	# Movimiento con WASD o flechas
-	if Input.is_action_pressed("ui_right"):
-		direction.x += 1
-	if Input.is_action_pressed("ui_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		direction.y += 1
-	if Input.is_action_pressed("ui_up"):
-		direction.y -= 1
-
-	# Normalizar vector para que no corra más rápido en diagonal
-	velocity = direction.normalized() * speed
-
-	# Mueve al personaje con colisiones
-	move_and_slide()
-	
 func _ready():
-	anim_sprite.play("default")
+	rotation=direction
 	
-	
+func _physics_process(delta):
+	velocity= Vector2(speed,0).rotated(direction)
+	move_and_slide()
